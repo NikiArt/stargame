b0074b0ab48afa9e90d982e2e5f96c61a8de05a7
NikiArt <genertyniki@gmail.com>
2018-11-23 20:50:51 +0300

    new project. Applyed changes of background and dimention

---
+++ desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.geekbrains.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 512;
+		config.height = 512;
+		new LwjglApplication(new StarGame(), config);
+	}
+}
