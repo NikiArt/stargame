754b1e0b8e2debcd3d414e11006afddc7579d2fc
NikiArt <genertyniki@gmail.com>
2018-12-10 15:37:17 +0300

    Add Game screen

---
+++ core/src/ru/geekbrains/sprite/PlayerShip.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.sprite;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import ru.geekbrains.base.Sprite;
+
+public class PlayerShip extends Sprite {
+
+    public PlayerShip(TextureRegion region) {
+        super(region);
+    }
+}
